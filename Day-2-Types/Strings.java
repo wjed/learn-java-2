abb7efd1317653d4f62f9ad9069d4814f3a5eb52
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-06-27 18:32:10 -0400

    End of Day 2

--- Day-2-Types/Strings.java
+++ Day-2-Types/Strings.java
@@ -1,9 +1,11 @@
 public class Strings {
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
+        String message = "    Hello World" + "!!";
         System.out.println(message.endsWith("!!"));
         System.out.println(message.length());
         System.out.println(message.indexOf("H"));
         System.out.println(message.replace("!", "*"));
+        System.out.println(message.trim());
+        System.out.println(message);
     }
 }
