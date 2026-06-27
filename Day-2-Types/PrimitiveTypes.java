f43605ffb851e05d2c5bc11852e015f9aee7f262
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-06-27 18:09:21 -0400

    Idk learning stuff

---
+++ Day-2-Types/PrimitiveTypes.java
@@ -0,0 +1,14 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        System.out.println(viewsCount);
+        float price = 10.99F;
+        System.out.println(price);
+        char letter = 'A';
+        System.out.println(letter);
+        boolean isEligible = false;
+        System.out.println(isEligible);
+    }
+    
+}
---
+++ Day-2-Types/PrimitiveVSReference.java
@@ -0,0 +1,8 @@
+public class PrimitiveVSReference {
+    public static void main (String[] args) {
+        byte x = 1;
+        byte y = x;
+        
+
+    }
+}
