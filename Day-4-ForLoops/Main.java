2f14701fc1ab92b0bdf8e6adcc860eec9e4a976d
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-07-05 18:23:54 -0400

    Breaking stuff

---
+++ Day-4-ForLoops/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello World" + " " + i);
+        
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Hello World with While Loop " + i);
+            i += 1;
+        }
+    }
+}
