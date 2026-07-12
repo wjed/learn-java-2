46d154224ff2c5fdb906ef0a39f41f4b561ab2e3
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-07-12 18:09:30 -0400

    GB Done

--- w3-schools/practice/GradeBook.java
+++ w3-schools/practice/GradeBook.java
@@ -1,5 +1,11 @@
 public class GradeBook {
     public static void main(String[] args) {
-        String[] TestScores = [85, 92, 78, 95, 88, 70]
+        int[] TestScores = {85, 92, 78, 95, 88, 70};
+        float sum = 0;
+        for (int score : TestScores) {
+            sum += score;
+        }
+        float myAverage = (sum / TestScores.length);
+        System.out.println("The average is " + myAverage);
     }
 }
